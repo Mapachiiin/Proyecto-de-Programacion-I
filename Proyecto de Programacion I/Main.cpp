fbaf48b3bf7126d33db856ab9de9dbade25d4b51
JP <josepablosanchezqwq@gmail.com>
2025-09-30 17:08:35 -0600

    Fin

--- Proyecto de Programacion I/Main.cpp
+++ Proyecto de Programacion I/Main.cpp
@@ -6,6 +6,11 @@ using namespace std;
 int main() {
 	Menu* menu = new Menu();
 	menu->quemados();
+	cout << "!!!Antes de iniciar el programa, por favor maximize la consola para una mejor experiencia." << endl << "Tambien, se advierte que todas las sucursales solamente tienen clientes, instructores y clases grupales quemadas." << endl<<" A excepcion de la SUC001, la cual se ingresaron datos mas profundos de los clientes y de las clases grupales.!!!";
+	cout << "---| Presione Enter para continuar |---" << endl;
+	cin.get();
+	system("cls");
+
 	menu->mostrarMenu();
 	
 
